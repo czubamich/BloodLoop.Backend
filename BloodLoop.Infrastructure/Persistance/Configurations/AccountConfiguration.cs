1b7dd6f8f1c5c8d3868d585091c2934b475a04de
Michael Czuba <czuba.mich@gmail.com>
2021-09-21 23:16:11 +0200

    Add initial migration

---
+++ BloodLoop.Infrastructure/Persistance/Configurations/AccountConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using BloodLoop.Domain;
+using BloodLoop.Domain.Accounts;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace BloodLoop.Infrastructure.Persistance.Configurations
+{
+    public class AccountConfiguration : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder
+               .ToTable($"{nameof(Account)}s");
+
+            builder
+                .Property(x => x.Id)
+                .HasConversion(x => x.Id, id => AccountId.Of(id));
+        }
+    }
+
+    public class IdentityRoletConfiguration : IEntityTypeConfiguration<IdentityRole<AccountId>>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole<AccountId>> builder)
+        {
+            builder
+               .ToTable("Roles");
+
+            builder
+                .Property(x => x.Id)
+                .HasConversion(x => x.Id, id => AccountId.Of(id));
+        }
+    }
+}
