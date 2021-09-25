48c93628160072b2d1125c13e04d4da5e47e956f
Michael Czuba <czuba.mich@gmail.com>
2021-09-25 12:14:22 +0200

    Add Extension methods, cleanup

---
+++ BloodCore/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodCore.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static bool PassedLongerThan(this DateTime date, DateTime then, TimeSpan time)
+        {
+            return (then - date).CompareTo(time) > 0;
+        }
+
+        public static TimeSpan DifferYears(this DateTime date, int years)
+        {
+            return DateTime.Now - DateTime.Now.AddYears(-years);
+        }
+    }
+}
---
+++ BloodCore/Extensions/GuardExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Ardalis.GuardClauses;
+using BloodCore.Common;
+using LanguageExt;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodCore.Extensions
+{
+    public static class GuardExtensions
+    {
+        public static TIdentity NullOrDefault<TIdentity>(this IGuardClause guardClause, [NotNull] TIdentity input, string parameterName, string? message = null) where TIdentity : Identity
+        {
+            Guard.Against.Null(input, parameterName);
+            if (input.Id.IsDefault())
+            {
+                throw new ArgumentException(message ?? $"Required input {parameterName} was empty.", parameterName);
+            }
+
+            return input;
+        }
+    }
+}
