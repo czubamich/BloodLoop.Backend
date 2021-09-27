43fdc3291505a6fb80bc813a7b75364b385cdcd3
Michael Czuba <czuba.mich@gmail.com>
2021-09-27 23:26:41 +0200

    Add Donors' Account Registration

---
+++ BloodLoop.Application/Accounts/Commands/RegisterDonor/RegisterDonorCommandHandler.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using BloodCore.Results;
+using BloodLoop.Application.Services;
+using BloodLoop.Application.Shared;
+using BloodLoop.Domain.Donors;
+using FluentValidation;
+using LanguageExt;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Unit = MediatR.Unit;
+
+namespace BloodLoop.Application.Accounts.Commands
+{
+    public class RegisterDonorCommandHandler : IRequestHandler<RegisterDonorCommand, Either<Error, DonorDto>>
+    {
+        private readonly IAccountService _accountService;
+        private readonly IDonorFactory _donorFactory;
+        private readonly IMapper _mapper;
+
+        public RegisterDonorCommandHandler(IAccountService accountService, IDonorFactory donorFactory, IMapper mapper)
+        {
+            _accountService = accountService;
+            _donorFactory = donorFactory;
+            _mapper = mapper;
+        }
+
+        public async Task<Either<Error, DonorDto>> Handle(RegisterDonorCommand request, CancellationToken cancellationToken)
+        {
+            var newDonor = _donorFactory.Create(
+                request.UserName, 
+                request.Email, 
+                request.FirstName, 
+                request.LastName,
+                request.Gender, 
+                request.BirthDay);
+
+            var identityResult = await _accountService.RegisterDonorAsync(newDonor, request.Password);
+
+            if (!identityResult.Succeeded)
+            {
+                var validationError = new Error.Validation();
+                AddErrorsFromIdentityResult(validationError, identityResult);
+
+                return validationError;
+            }
+
+            return _mapper.Map<DonorDto>(newDonor);
+        }
+
+        private void AddErrorsFromIdentityResult(Error.Validation validationError, IdentityResult identityResult)
+        {
+            var errors = 
+                identityResult.Errors.Select(x => new
+                {
+                    PropertyName = MapIdentityErrorToPropertyName(x), 
+                    Error = x.Description
+                });
+
+            foreach(var error in errors)
+            {
+                validationError.AddError(error.PropertyName, error.Error);
+            }
+        }
+
+        private string MapIdentityErrorToPropertyName(IdentityError identityError)
+        {
+            switch (identityError.Code)
+            {
+                case nameof(IdentityErrorDescriber.InvalidUserName):
+                case nameof(IdentityErrorDescriber.DuplicateUserName):
+                    return nameof(RegisterDonorCommand.UserName);
+                case nameof(IdentityErrorDescriber.InvalidEmail):
+                case nameof(IdentityErrorDescriber.DuplicateEmail):
+                    return nameof(RegisterDonorCommand.Email);
+                case nameof(IdentityErrorDescriber.PasswordRequiresDigit):
+                case nameof(IdentityErrorDescriber.PasswordRequiresLower):
+                case nameof(IdentityErrorDescriber.PasswordRequiresNonAlphanumeric):
+                case nameof(IdentityErrorDescriber.PasswordRequiresUpper):
+                case nameof(IdentityErrorDescriber.PasswordTooShort):
+                case nameof(IdentityErrorDescriber.PasswordRequiresUniqueChars):
+                    return nameof(RegisterDonorCommand.Password);
+                default:
+                    return string.Empty;
+            }
+        }
+    }
+}
---
+++ BloodLoop.Application/Shared/DonorProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+using AutoMapper;
+using BloodLoop.Domain.Donors;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Routing.Constraints;
+
+namespace BloodLoop.Application.Shared
+{
+    public class DonorProfile : Profile
+    {
+        public DonorProfile()
+        {
+            CreateMap<Donor, DonorDto>()
+                .ForMember(dst => dst.Id, opt => opt.MapFrom(src => src.Id.Id));
+        }
+    }
+}
