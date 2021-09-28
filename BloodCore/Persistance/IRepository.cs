28308a1d85a28c6ef95d9a246161199da92677a6
Michael Czuba <czuba.mich@gmail.com>
2021-09-29 01:25:21 +0200

    Add AutoMapper result projection to EfRepository

--- BloodCore/Persistance/IRepository.cs
+++ BloodCore/Persistance/IRepository.cs
@@ -1,4 +1,5 @@
-﻿using BloodCore.Domain;
+﻿using System;
+using BloodCore.Domain;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,10 +19,14 @@ namespace BloodCore.Persistance
         Task<TAggregate> GetById(TIdentity id, CancellationToken cancellationToken = default);
 
         Task<TAggregate> Get(ISpecification<TAggregate> specification, CancellationToken cancellationToken = default);
+        [Obsolete("Use FindAs instead for automapper")]
         Task<TResult> Get<TResult>(ISpecification<TAggregate, TResult> specification, CancellationToken cancellationToken = default);
+        Task<TResult> GetAs<TResult>(ISpecification<TAggregate> specification, CancellationToken cancellationToken = default);
 
         Task<IReadOnlyList<TAggregate>> Find(ISpecification<TAggregate> specification, CancellationToken cancellationToken = default);
+        [Obsolete("Use FindAs instead for automapper")]
         Task<IReadOnlyList<TResult>> Find<TResult>(ISpecification<TAggregate, TResult> specification, CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<TResult>> FindAs<TResult>(ISpecification<TAggregate> specification, CancellationToken cancellationToken = default);
 
         Task<TAggregate> AddAsync(TAggregate entity, CancellationToken cancellationToken = default);
         Task AddRangeAsync(IEnumerable<TAggregate> entities, CancellationToken cancellationToken = default);
--- BloodLoop.Infrastructure/Persistance/Common/EfRepository.cs
+++ BloodLoop.Infrastructure/Persistance/Common/EfRepository.cs
@@ -22,30 +22,38 @@ namespace BloodLoop.Infrastructure.Persistance
         protected readonly ApplicationDbContext DbContext;
         protected readonly DbSet<TAggregate> Aggregates;
         protected readonly ISpecificationEvaluator SpecificationEvaluator;
+        private readonly IMapper _mapper;
 
         public IUnitOfWork UnitOfWork => DbContext as IUnitOfWork;
 
-        public EfRepository(ApplicationDbContext dbContext, ISpecificationEvaluator specificationEvaluator)
+        public EfRepository(ApplicationDbContext dbContext, ISpecificationEvaluator specificationEvaluator, IMapper mapper)
         {
             DbContext = dbContext;
             Aggregates = dbContext.Set<TAggregate>();
             SpecificationEvaluator = specificationEvaluator;
+            _mapper = mapper;
         }
 
         public async Task<TAggregate> GetById(TIdentity id, CancellationToken cancellationToken = default)
             => await Aggregates.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
 
         public async Task<TAggregate> Get(ISpecification<TAggregate> specification, CancellationToken cancellationToken = default)
-            => await ApplySpecification(specification).FirstOrDefaultAsync(cancellationToken);
+            => await Aggregates.WithSpecification(specification).SingleOrDefaultAsync(cancellationToken);
 
         public async Task<TResult> Get<TResult>(ISpecification<TAggregate, TResult> specification, CancellationToken cancellationToken = default)
             => await ApplySpecification(specification).FirstOrDefaultAsync(cancellationToken);
 
+        public async Task<TResult> GetAs<TResult>(ISpecification<TAggregate> specification, CancellationToken cancellationToken = default)
+            => await Aggregates.AsNoTracking().WithSpecification(specification).ProjectTo<TResult>(_mapper.ConfigurationProvider).SingleOrDefaultAsync(cancellationToken);
+
         public async Task<IReadOnlyList<TAggregate>> Find(ISpecification<TAggregate> specification, CancellationToken cancellationToken = default)
-            => await ApplySpecification(specification).ToListAsync(cancellationToken);
+            => (await Aggregates.ToListAsync(specification, cancellationToken)).ToList();
 
         public async Task<IReadOnlyList<TResult>> Find<TResult>(ISpecification<TAggregate, TResult> specification, CancellationToken cancellationToken = default)
             => await ApplySpecification(specification).ToListAsync(cancellationToken);
+
+        public async Task<IReadOnlyList<TResult>> FindAs<TResult>(ISpecification<TAggregate> specification, CancellationToken cancellationToken = default)
+            => await Aggregates.AsNoTracking().WithSpecification(specification).ProjectTo<TResult>(_mapper.ConfigurationProvider).ToListAsync(cancellationToken);
 
         public async Task AddRangeAsync(IEnumerable<TAggregate> entities, CancellationToken cancellationToken = default)
             => await Aggregates.AddRangeAsync(entities, cancellationToken);
